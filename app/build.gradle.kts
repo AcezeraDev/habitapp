b9534681301cba3c179db446bfa7cfe4c799ef62
AcezeraDev <acezera333@gmail.com>
2026-05-14 19:30:04 -0300

    Add HabitApp v2.4 debug APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.exemple.habitapp"
         minSdk = 21
         targetSdk = 36
-        versionCode = 12
-        versionName = "2.3"
+        versionCode = 13
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
